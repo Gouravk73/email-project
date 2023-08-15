e867b93d82425f6f3c315f7252852c72743cf71c
Gourav <Gouravkumar.1273@gmail.com>
2023-08-16 00:03:34 +0530

    login pasge implement

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './components/Home';
 import SignIn from './components/SignIn';
+import Login from './components/Login';
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
     </BrowserRouter>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,17 @@
 import React, { useRef } from 'react'
-import { Button, Card } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-const SignIn = () => {
+const Login = () => {
     const emailInput=useRef();
     const PasswordInput=useRef();
-    const confirmPasswordInput=useRef();
-
     const navigate =useNavigate()
 
     const submitHandler =async (e) => {
         e.preventDefault();
         const email=emailInput.current.value ;
         const password=PasswordInput.current.value ;
-        const confirmPassword=confirmPasswordInput.current.value ;
-        if(confirmPassword!==password) {
-            alert('password not match')
-            return 
-        }
-        else{
-            try{const res=await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA3nj_bUobX7NvwdKCpIb3QUm24_dlqbWk`,{
+          
+            try{const res=await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA3nj_bUobX7NvwdKCpIb3QUm24_dlqbWk`,{
                 method:"POST",
                 body:JSON.stringify({
                     email,
@@ -34,20 +26,24 @@ const SignIn = () => {
 
             }
             const data=await res.json()
+            localStorage.setItem("email",  data.email)
+            localStorage.setItem("token",  data.idToken)
             console.log(data)
+            navigate('/')
+
         }
             catch(e){
                 alert(e)
             }
-        }
+ 
 
     }
   return (
     <div className='container-fluid  d-inline-block   d-flex justify-content-center align-items-center' style={{border:'1px solid red',height:'100vh'}}> 
         <div className='row  '>
 
-            <div className="col p-5"style={{border:'1px solid black',borderRadius:'20px'}}>
-                <h1 className='text-center'>Sign Up</h1>
+            <div className="col p-5 text-center"style={{border:'1px solid black',borderRadius:'20px'}}>
+                <h1 className='text-center'>Login</h1>
                 <form onSubmit={submitHandler}>
                     <div className='form-floating mb-3'>
                         <input type="email" className="form-control"   placeholder='abc@gmail.com'ref={emailInput} />
@@ -58,23 +54,20 @@ const SignIn = () => {
                         <input type="password" className="form-control"   placeholder='abc@ .com' ref={PasswordInput} />
                         <label htmlFor="">Password</label>
 
-                    </div>
-                    <div className='form-floating mb-3'>
-                        <input type="password" className="form-control"  placeholder='abc@gmail.com'ref={confirmPasswordInput} />
-                        <label htmlFor="">Confirm Password</label>
-
-                    </div>
-                    <button className='btn btn-primary' >submit</button>
+                    </div> 
+                    <button className='btn btn-primary' >Login</button>
                 </form>
+                    <Link>Forget Password</Link>
                 <hr />
-                <div className='row mb-3'>
-                    <button className='btn btn-success' onClick={()=>navigate('/login')}>Have an account already ?</button>
-                </div>
+                
             </div>
+            <div className='row my-5 ' >
+                    <button className='btn btn-success' onClick={()=>navigate('/signin')}>Don't Have an account   ? Sign Up</button>
+                </div>
         </div>
 
     </div>
   )
 }
 
-export default SignIn
+export default Login
